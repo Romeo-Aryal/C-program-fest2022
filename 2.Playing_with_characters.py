fc94371a50d5510430d22c127a7b552dda9479e1
unknown <kcroshan682@gmail.com>
2022-10-11 23:54:54 +0545

    done via python

---
+++ 2.Playing_with_characters.py
@@ -0,0 +1,10 @@
+ch = input("Enter the character : ")
+s = input("Enter the string : ")
+# print the character
+print(ch)
+#save in another var
+s_tmp = s
+# print the string
+print(s)
+print(s_tmp)
+
