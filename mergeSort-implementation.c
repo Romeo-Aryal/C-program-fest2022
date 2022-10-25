aca5008287a170025a3a70246798a12bb2cce412
Rachanagyawali <84080537+Rachanagyawali@users.noreply.github.com>
2022-10-25 09:36:22 -0700

    implementation of merge sort in c

---
+++ mergeSort-implementation.c
@@ -0,0 +1,43 @@
+#include<stdlib.h>
+#include<stdio.h>
+// Merge Function
+void merge(int arr[], int l, int m, int r)
+{
+int i, j, k;
+int n1 = m - l + 1;
+int n2 = r - m;
+int L[n1], R[n2];
+for (i = 0; i < n1; i++)
+L[i] = arr[l + i];
+for (j = 0; j < n2; j++)
+R[j] = arr[m + 1+ j];
+i = 0;
+j = 0;
+k = l;
+while (i < n1 && j < n2)
+{
+if (L[i] <= R[j])
+{
+arr[k] = L[i];
+i++;
+}
+else
+{
+arr[k] = R[j];
+j++;
+}
+k++;
+}
+while (i < n1)
+{
+arr[k] = L[i];
+i++;
+k++;
+}
+while (j < n2)
+{
+arr[k] = R[j];
+j++;
+k++;
+}
+}
