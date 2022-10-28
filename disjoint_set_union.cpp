ea4cffad0b6c703dd5ebaf9bffb345bd06d495bf
Tarun Singh <tarunsinghofficial236@gmail.com>
2022-10-28 11:01:12 +0530

    Add a program for disjoint set union.

--- disjoint_set_union.cpp
+++ disjoint_set_union.cpp
@@ -1,6 +1,51 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
+typedef long long int ll;
+#define M 1000000007
 using namespace std;
 
-int main(){
-    
+ll parent[100005];
+ll sz[100005];
+
+ll findParent(ll node)
+{
+    if (parent[node] == node)
+    {
+        return node;
+    }
+    return parent[node] = findParent(parent[node]);
+}
+
+void uni(ll u, ll v)
+{
+    ll paru = findParent(u);
+    ll parv = findParent(v);
+    if (sz[paru] < sz[parv])
+    {
+        parent[paru] = parv;
+        sz[parv] += sz[paru];
+    }
+    else
+    {
+        parent[parv] = paru;
+        sz[paru] += sz[parv];
+    }
+}
+
+void init(ll n)
+{
+    for (ll i = 1; i <= n; i++)
+    {
+        parent[i] = i;
+        sz[i] = 1;
+    }
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    ll n;
+    cin >> n;
+    init(n);
+    return 0;
 }
